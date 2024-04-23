acf2634b2f192dc901d94284f4ad54c41f95581d
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-04-24 02:16:43 +0900

    [Feat]#6-Add SearchPageVc, Storyboard

---
+++ PopDangSquare/Controller/SearchPageVC.swift
@@ -0,0 +1,29 @@
+//
+//  SearchPageVC.swift
+//  PopDangSquare
+//
+//  Created by 한철희 on 4/24/24.
+//
+
+import UIKit
+
+class SearchPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
