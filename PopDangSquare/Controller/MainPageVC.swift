b235f65276bccf8e5503dc904c5580e32b19e5d3
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-04-23 15:01:07 +0900

    [Feat]#5-Add Scroll View

---
+++ PopDangSquare/Controller/MainPageVC.swift
@@ -0,0 +1,29 @@
+//
+//  MainPageVC.swift
+//  PopDangSquare
+//
+//  Created by 한철희 on 4/23/24.
+//
+
+import UIKit
+
+class MainPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
