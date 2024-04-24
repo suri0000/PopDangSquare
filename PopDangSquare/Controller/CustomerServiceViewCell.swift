8dab571344113bf2f81955ea68fcdc2a652ca93d
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-04-24 23:05:29 +0900

    Bc
    Bc

---
+++ PopDangSquare/Controller/CustomerServiceViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomerServiceViewCell.swift
+//  PopDangSquare
+//
+//  Created by Developer_P on 4/24/24.
+//
+
+import UIKit
+
+class CustomerServiceViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
