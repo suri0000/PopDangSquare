ce2360eb6ca4473ff27845ad6ef92bab556441ec
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-04-24 11:23:42 +0900

    [Feat]#6-Add 검색 결과에 따른 label 히든처리

--- PopDangSquare/Controller/SearchPageVC.swift
+++ PopDangSquare/Controller/SearchPageVC.swift
@@ -7,16 +7,30 @@
 
 import UIKit
 
-class SearchPageVC: UIViewController {
+class SearchPageVC: UIViewController, UISearchBarDelegate {
     // 추후 네비게이션 연결 후 필요한 코드
-//    .navigationBarBackButtonHidden()
-
+    //    .navigationBarBackButtonHidden()
+    
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchResult: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.searchBarStyle = .minimal
         searchBar.searchTextField.backgroundColor = .white
+        searchResult.isHidden = true // 초기에는 검색 결과 레이블을 숨김
+        searchBar.delegate = self
     }
-
+    
+    // 검색 버튼을 눌렀을 때 호출되는 메서드
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder() // 키보드 숨기기
+        if let searchText = searchBar.text, !searchText.isEmpty {
+            searchResult.text = "\(searchText)로 검색된 결과입니다."
+            searchResult.isHidden = false // 검색 결과 레이블 보이기
+        } else {
+            searchResult.isHidden = true // 검색 결과 레이블 숨기기
+        }
+    }
+    
 }
