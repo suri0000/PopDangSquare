88576ad5e25c06f0c4744554018a114f2bc7ffc0
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-04-25 23:49:35 +0900

    [Feat]#11-MyPage-Edit-My-Information-UI
    MyPageInformationVC - Modify

--- PopDangSquare/Controller/MyPageInformationVC.swift
+++ PopDangSquare/Controller/MyPageInformationVC.swift
@@ -9,6 +9,22 @@ import Foundation
 import UIKit
 
 class MyPageInformationVC: UIViewController {
+    // 마이페이지 배경 연결부
+    @IBOutlet weak var myPageInfoBackgroundImage: UIImageView!
+    @IBOutlet weak var myPageInfoBackgroundButton: UIButton!
+    
+    // 마이페이지 프로필 연결부
+    @IBOutlet weak var myPageInfoPorfileImage: UIImageView!
+    @IBOutlet weak var myPageInfoProfileButton: UIButton!
+    
+    // 마이페이지 레이블 연결부
+    @IBOutlet weak var myPageInfoNameLable: UILabel!
+    
+    @IBOutlet weak var myPageInfoNameModify: UIButton!
+    @IBOutlet weak var myPageInfoEmailLable: UILabel!
+    @IBOutlet weak var myPageInfoEmailModify: UIButton!
+    @IBOutlet weak var myPageInfoPasswordModify: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
