d81252dfe3e5cccdc3ac054ff7df86010c8edb95
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-04-25 21:38:02 +0900

    [Feat]#10-기본 탭바 완성

--- PopDangSquare/AppDelegate.swift
+++ PopDangSquare/AppDelegate.swift
@@ -10,41 +10,12 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    var window: UIWindow?
+
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        // 윈도우 생성
-        window = UIWindow(frame: UIScreen.main.bounds)
-        
-        // UITabBarController 인스턴스 생성
-        let tabBarController = UITabBarController()
-        
-        // 첫 번째 탭: MainPage
-        let mainPageVC = MainPageVC() // MainPageViewController는 사용자가 정의한 뷰 컨트롤러 클래스 이름으로 가정합니다.
-        mainPageVC.tabBarItem = UITabBarItem(title: "홈", image: UIImage(named: "home_icon"), selectedImage: UIImage(named: "home_icon_selected"))
-        
-        // 두 번째 탭: SearchPage
-        let searchPageVC = SearchPageVC() // SearchPageViewController는 사용자가 정의한 뷰 컨트롤러 클래스 이름으로 가정합니다.
-        searchPageVC.tabBarItem = UITabBarItem(title: "검색", image: UIImage(named: "search_icon"), selectedImage: UIImage(named: "search_icon_selected"))
-        
-        // 세 번째 탭: DetailView
-        let detailViewVC = DetailViewController() // DetailViewController는 사용자가 정의한 뷰 컨트롤러 클래스 이름으로 가정합니다.
-        detailViewVC.tabBarItem = UITabBarItem(title: "준비중", image: UIImage(named: "detail_icon"), selectedImage: UIImage(named: "detail_icon_selected"))
-        
-        // 네 번째 탭: MyPage
-        let myPageVC = MyPageLoginController() // MyPageViewController는 사용자가 정의한 뷰 컨트롤러 클래스 이름으로 가정합니다.
-        myPageVC.tabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(named: "mypage_icon"), selectedImage: UIImage(named: "mypage_icon_selected"))
-        
-        // 탭 바 컨트롤러에 뷰 컨트롤러 배열 추가
-        tabBarController.viewControllers = [mainPageVC, searchPageVC, detailViewVC, myPageVC]
-        
-        // 탭 바 컨트롤러를 루트 뷰 컨트롤러로 설정
-        window?.rootViewController = tabBarController
-        window?.makeKeyAndVisible()
-        
+        // Override point for customization after application launch.
         return true
-    }
+      }
     
     // MARK: UISceneSession Lifecycle
     
--- PopDangSquare/View/TabBar/TabBarVC.swift
+++ PopDangSquare/View/TabBar/TabBarVC.swift
@@ -1,35 +1,35 @@
-////
-////  TabBarVC.swift
-////  PopDangSquare
-////
-////  Created by 한철희 on 4/25/24.
-////
 //
-//import UIKit
+//  TabBarVC.swift
+//  PopDangSquare
 //
-//class TabBarVC: UITabBarController {
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        // MainPage 뷰 컨트롤러 생성 및 설정
-//        let mainPageVC = UIStoryboard(name: "MainPage", bundle: nil).instantiateViewController(withIdentifier: "MainPage")
-//        mainPageVC.tabBarItem = UITabBarItem(title: "홈", image: nil, selectedImage: nil)
-//        
-//        // SearchPage 뷰 컨트롤러 생성 및 설정
-//        let searchPageVC = UIStoryboard(name: "SearchPage", bundle: nil).instantiateViewController(withIdentifier: "SearchPage")
-//        searchPageVC.tabBarItem = UITabBarItem(title: "검색", image: nil, selectedImage: nil)
-//        
-//        // DetailView 뷰 컨트롤러 생성 및 설정
-//        let detailViewVC = UIStoryboard(name: "DetailView", bundle: nil).instantiateViewController(withIdentifier: "DetailViewController")
-//        detailViewVC.tabBarItem = UITabBarItem(title: "준비중", image: nil, selectedImage: nil)
-//        
-//        // MyPage 뷰 컨트롤러 생성 및 설정
-//        let myPageVC = UIStoryboard(name: "MyPage", bundle: nil).instantiateViewController(withIdentifier: "MyPage")
-//        myPageVC.tabBarItem = UITabBarItem(title: "마이페이지", image: nil, selectedImage: nil)
-//        
-//        // 생성된 뷰 컨트롤러들을 탭 바 컨트롤러에 추가
-//        viewControllers = [mainPageVC, searchPageVC, detailViewVC, myPageVC]
-//    }
-//    
-//}
+//  Created by 한철희 on 4/25/24.
+//
+
+import UIKit
+
+class TabBarVC: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //        // MainPage 뷰 컨트롤러 생성 및 설정
+        //        let mainPageVC = UIStoryboard(name: "MainPage", bundle: nil).instantiateViewController(withIdentifier: "MainPage")
+        //        mainPageVC.tabBarItem = UITabBarItem(title: "홈", image: nil, selectedImage: nil)
+        //        
+        //        // SearchPage 뷰 컨트롤러 생성 및 설정
+        //        let searchPageVC = UIStoryboard(name: "SearchPage", bundle: nil).instantiateViewController(withIdentifier: "SearchPage")
+        //        searchPageVC.tabBarItem = UITabBarItem(title: "검색", image: nil, selectedImage: nil)
+        //        
+        //        // DetailView 뷰 컨트롤러 생성 및 설정
+        //        let detailViewVC = UIStoryboard(name: "DetailView", bundle: nil).instantiateViewController(withIdentifier: "DetailViewController")
+        //        detailViewVC.tabBarItem = UITabBarItem(title: "준비중", image: nil, selectedImage: nil)
+        //        
+        //        // MyPage 뷰 컨트롤러 생성 및 설정
+        //        let myPageVC = UIStoryboard(name: "MyPage", bundle: nil).instantiateViewController(withIdentifier: "MyPage")
+        //        myPageVC.tabBarItem = UITabBarItem(title: "마이페이지", image: nil, selectedImage: nil)
+        //        
+        //        // 생성된 뷰 컨트롤러들을 탭 바 컨트롤러에 추가
+        //        viewControllers = [mainPageVC, searchPageVC, detailViewVC, myPageVC]
+        //    }
+    }
+}
