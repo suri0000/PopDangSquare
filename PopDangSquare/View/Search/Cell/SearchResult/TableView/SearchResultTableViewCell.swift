0dc5068e111b42f6c2260b9440e4747d90ed80b5
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-04-26 02:10:43 +0900

    [Feat]#24-컬렉션뷰 삭제

--- PopDangSquare/View/Search/Cell/SearchResult/TableView/SearchResultTableViewCell.swift
+++ PopDangSquare/View/Search/Cell/SearchResult/TableView/SearchResultTableViewCell.swift
@@ -8,37 +8,15 @@
 import UIKit
 
 class SearchResultTableViewCell: UITableViewCell {
-
-    @IBOutlet weak var collectionView: UICollectionView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        
-        collectionView.register(UINib(nibName: "SearchResultCell", bundle: nil), forCellWithReuseIdentifier: "SearchResultCell")
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        
     }
-    
-}
-extension SearchResultTableViewCell: UICollectionViewDataSource, UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchResultCell", for: indexPath) as! SearchResultCell
-        
-        //        cell.configure(with: model[indexPath.row])
-        return cell
-    }
-    
     
 }
