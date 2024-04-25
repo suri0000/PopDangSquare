527159adbf12bfb62beb16ede07c5bc6a93e410a
kimminung <minogo1054@paran.com>
2024-04-26 01:11:09 +0900

    [Feat]-detail 외형 70%

---
+++ PopDangSquare/View/Reserve/ReserveCell.swift
@@ -0,0 +1,17 @@
+//
+//  ReserveCell.swift
+//  PopDangSquare
+//
+//  Created by 민웅킴 on 4/26/24.
+//
+
+import UIKit
+
+class ReserveCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
