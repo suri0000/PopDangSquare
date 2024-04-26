12211a788a402ac5e435076ed1b06e6afcb7ee98
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-04-27 01:27:15 +0900

    [Feat]33-셀관련 파일들 정리

--- PopDangSquare/View/Main/Cell/MovieChart/CollectionView/MovieChartCell.swift
+++ PopDangSquare/View/Main/Cell/MovieChart/CollectionView/MovieChartCell.swift
@@ -11,7 +11,28 @@ class MovieChartCell: UICollectionViewCell {
     
     @IBOutlet weak var chartImage: UIImageView!
     
-    // 이미지 이름을 받아서 이미지 뷰에 설정하는 메소드
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupShadow()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateShadowPath()
+    }
+    
+    private func setupShadow() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        layer.shadowRadius = 4
+        layer.shadowOpacity = 0.5
+        layer.masksToBounds = false
+    }
+    
+    private func updateShadowPath() {
+        layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: contentView.layer.cornerRadius).cgPath
+    }
+    
     func configure(with movie: Popular) {
         if let posterPath = movie.posterPath, let url = URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)") {
             URLSession.shared.dataTask(with: url) { data, response, error in
@@ -23,31 +44,5 @@ class MovieChartCell: UICollectionViewCell {
             }.resume()
         }
     }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        setupShadow()
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        updateShadowPath()
-    }
-    
-    /// 그림자 설정을 위한 메서드입니다.
-    private func setupShadow() {
-        layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOffset = CGSize(width: 0, height: 2)
-        layer.shadowRadius = 4
-        layer.shadowOpacity = 0.5
-        layer.masksToBounds = false
-    }
-    
-    /// 그림자 경로를 셀의 최종 크기에 맞게 업데이트합니다.
-    private func updateShadowPath() {
-        layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: contentView.layer.cornerRadius).cgPath
-    }
-    
-    
     
 }
--- PopDangSquare/View/Main/Cell/MovieChart/TableView/MovieChartTableViewCell.swift
+++ PopDangSquare/View/Main/Cell/MovieChart/TableView/MovieChartTableViewCell.swift
@@ -16,55 +16,59 @@ class MovieChartTableViewCell: UITableViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        self.backgroundColor = UIColor.clear
+        
+        backgroundColor = .clear
         collectionView.layer.cornerRadius = 10
         contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 12, right: 0))
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // 콜렉션뷰 레이아웃 설정
-        self.selectionStyle = .none
+        selectionStyle = .none
+        
+        configureCollectionView()
+        setupPageControl()
+        fetchMovies()
+    }
+    
+    private func configureCollectionView() {
         let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal // 가로 스크롤 설정
-        layout.itemSize = CGSize(width: 360, height: 200) // 셀 크기 설정
-        layout.minimumLineSpacing = 0 // 셀 간 최소 간격 설정
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 360, height: 200)
+        layout.minimumLineSpacing = 0
         
         collectionView.setCollectionViewLayout(layout, animated: false)
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(UINib(nibName: "MovieChartCell", bundle: nil), forCellWithReuseIdentifier: "MovieChartCell")
-        collectionView.isPagingEnabled = true // 페이징 활성화
-        
-        setupPageControl()
-        
-        // 영화 데이터 불러오기
-        fetchMovies()
+        collectionView.isPagingEnabled = true
     }
     
-    func setupPageControl() {
+    private func setupPageControl() {
         let leftMargin: CGFloat = -9
         let topMargin: CGFloat = 30
-        pageControl = UIPageControl(frame: CGRect(x: leftMargin, y: collectionView.frame.minY + topMargin, width: 100, height: 20))
+        
+        pageControl = UIPageControl()
         pageControl.numberOfPages = movies.count
         pageControl.currentPage = 0
-        pageControl.tintColor = UIColor.red
-        pageControl.pageIndicatorTintColor = UIColor.lightGray
-        pageControl.currentPageIndicatorTintColor = UIColor.black
-        self.contentView.addSubview(pageControl)
+        pageControl.tintColor = .red
+        pageControl.pageIndicatorTintColor = .lightGray
+        pageControl.currentPageIndicatorTintColor = .black
+        
+        contentView.addSubview(pageControl)
         pageControl.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
-            pageControl.leftAnchor.constraint(equalTo: self.contentView.leftAnchor, constant: leftMargin),
-            pageControl.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: topMargin),
+            pageControl.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: leftMargin),
+            pageControl.topAnchor.constraint(equalTo: contentView.topAnchor, constant: topMargin),
             pageControl.heightAnchor.constraint(equalToConstant: 20)
         ])
     }
     
-    func fetchMovies() {
+    private func fetchMovies() {
         PopularManager.shared.fetchMovies { [weak self] (movies, error) in
             DispatchQueue.main.async {
                 if let movies = movies {
-                    // 배열에서 최대 5개의 요소만 남깁니다.
                     let firstFiveMovies = Array(movies.prefix(5))
                     self?.movies = firstFiveMovies
                     self?.collectionView.reloadData()
@@ -78,14 +82,19 @@ class MovieChartTableViewCell: UITableViewCell {
 }
 
 extension MovieChartTableViewCell: UICollectionViewDataSource, UICollectionViewDelegate {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return movies.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieChartCell", for: indexPath) as! MovieChartCell
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieChartCell", for: indexPath) as? MovieChartCell else {
+            fatalError("Unable to dequeue MovieChartCell")
+        }
+        
         let movie = movies[indexPath.row]
         cell.configure(with: movie)
+        
         return cell
     }
     
@@ -95,3 +104,4 @@ extension MovieChartTableViewCell: UICollectionViewDataSource, UICollectionViewD
         pageControl.currentPage = pageIndex
     }
 }
+
